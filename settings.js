121d0faa667aa6f17fdb766d9b8f79dc7c77d767
wanzz25 <wanzzganzz095@gmail.com>
2026-09-25 17:25:39 +0700

    Add settings.js via Wanzz Deploy

---
+++ settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  creatorName: "Wanz",
+  apiTitle: "Wanz Api Hub",
+  webName: "Wanz Api",
+  favicon: "https://cdn-alip.clutch.web.id/api/u/mr1qezqk.jpg",
+  logoIcon: "⚡",
+  logoIconUrl: "https://cdn-alip.clutch.web.id/api/u/mr1qezqk.jpg",
+  whatsappLink: "https://wa.me/6281578141066",
+  githubLink: "https://github.com/alifalfrl",
+  instagramLink: "https://www.instagram.com/alifalfrl__?igsh=OG1jbjdubnIwOWtl",
+  telegramLink: "https://t.me/bigedikeman",
+  whatsappChannelLink: "https://whatsapp.com/channel/0029VbCvj6M1SWt9m4Zd0u2P",
+  emailAddress: "alifalfarel10@gmail.com",
+  apiKeys: ["wanz"]
+};
